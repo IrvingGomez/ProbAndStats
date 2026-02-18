54dec61814c92832c3869b69ee23b607379e9bd3
Irving Gomez <irvingesfm@gmail.com>
2026-02-18 15:25:10 +0700

    Added Citation and License, v0.1.1

--- ui/version.py
+++ ui/version.py
@@ -1,5 +1,5 @@
 # ui/version.py
 APP_NAME = "Thotsakan Statistics"
 APP_TAGLINE = "Probability and Statistics Interactive Laboratory"
-APP_VERSION = "0.1.0"
+APP_VERSION = "0.1.1"
 APP_RELEASE_DATE = "2026-02-18"  # ISO format, optional
